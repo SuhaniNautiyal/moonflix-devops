4cb8aa813885a7fff635be8b14db651faeff26a9
Suhani Nautiyal <suhaninautiyal02@gmail.com>
2025-12-24 15:46:01 +0530

    fixed tmdb

--- backend/src/axios/axios.client.js
+++ backend/src/axios/axios.client.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
+const axiosClient = axios.create({
+  baseURL: process.env.TMDB_BASE_URL,
+  params: {
+    api_key: process.env.TMDB_API_KEY
+  },
+  headers: {
+    Accept: "application/json",
+    "Accept-Encoding": "identity"
+  }
+});
+
 const get = async (url) => {
-  const response = await axios.get(url, {
-    headers: {
-      Accept: "application/json",
-      "Accept-Encoding": "identity"
-    }
-  });
+  const response = await axiosClient.get(url);
   return response.data;
 };
 
